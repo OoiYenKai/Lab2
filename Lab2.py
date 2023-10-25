f8fa2e6699db90bc463bee6c19f95a76200959f4
Ooi Yen Kai <yenkai.22@ichat.sp.edu.sg>
2023-10-25 17:27:20 +0800

    Lab 2 extra exercise

--- Lab2.py
+++ Lab2.py
@@ -25,8 +25,21 @@ def calc_min_max_temperature(final_user_inputs):
     print([int(min_temp), int(max_temp)])
     return [int(min_temp), int(max_temp)]
 
+def calc_median_temperature(final_user_inputs):
+    sorted_temperatures = sorted(final_user_inputs)
+    amtOfNumbers = len(sorted_temperatures)
+    if amtOfNumbers % 2 == 0:
+        evenNum1 = sorted_temperatures[amtOfNumbers // 2 - 1]
+        evenNum2 = sorted_temperatures[amtOfNumbers // 2]
+        median = (evenNum1 + evenNum2) / 2
+    else:
+        median = sorted_temperatures[amtOfNumbers // 2]
+    print(median)
+    return median
+
 if __name__ == "__main__":
     display_main_menu()
     final_user_inputs = get_user_input()
     calc_average_temperature(final_user_inputs)
     calc_min_max_temperature(final_user_inputs)
+    calc_median_temperature(final_user_inputs)
